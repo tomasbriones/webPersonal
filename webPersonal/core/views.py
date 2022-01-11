bebaa9fba585a3a13beeeeff908ec6c348997da7
TBRIONES-HP450\Administrador <tbriones@economia.cl>
2022-01-11 16:05:52 -0300

    add home

--- webPersonal/core/views.py
+++ webPersonal/core/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
-# Create your views here.
+def home(request):
+    html_response = "<h1>Mi web personal</h1><h2>portada</h2>"
+    return HttpResponse(html_response)
