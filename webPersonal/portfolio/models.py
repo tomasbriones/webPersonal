2aeb4ab8d5e7b0559140a7fb9f018cdcab36ac17
TBRIONES-HP450\Administrador <tbriones@economia.cl>
2022-01-12 18:22:00 -0300

    migrate admin and model portfolio edit 1

--- webPersonal/portfolio/models.py
+++ webPersonal/portfolio/models.py
@@ -2,8 +2,17 @@ from django.db import models
 
 # Create your models here.
 class Project(models.Model):
-    title = models.CharField(max_length=200)
-    desccription = models.TextField()
-    image = models.ImageField()
-    created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=200,verbose_name = 'titulo')
+    desccription = models.TextField(verbose_name = 'descripcion')
+    image = models.ImageField(verbose_name = 'imagen')
+    created = models.DateTimeField(auto_now_add=True,verbose_name = 'fecha creacion')
+    updated = models.DateTimeField(auto_now_add=True,verbose_name = 'fecha actualizacion')
+
+    class Meta:
+        verbose_name = 'proyecto'
+        verbose_name_plural = 'proyectos'
+        ordering = ['-created']
+
+    def __str__(self):
+        return self.title
+    
