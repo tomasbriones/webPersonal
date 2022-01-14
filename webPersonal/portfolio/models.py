bb661a34676462f31e221b7cd6343b6915cd4a3c
TBRIONES-HP450\Administrador <tbriones@economia.cl>
2022-01-14 09:53:17 -0300

    add media files

--- webPersonal/portfolio/models.py
+++ webPersonal/portfolio/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Project(models.Model):
     title = models.CharField(max_length=200,verbose_name = 'titulo')
     desccription = models.TextField(verbose_name = 'descripcion')
-    image = models.ImageField(verbose_name = 'imagen')
+    image = models.ImageField(verbose_name = 'imagen', upload_to = 'projects')
     created = models.DateTimeField(auto_now_add=True,verbose_name = 'fecha creacion')
     updated = models.DateTimeField(auto_now_add=True,verbose_name = 'fecha actualizacion')
 
